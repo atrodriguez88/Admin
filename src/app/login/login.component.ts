a7024b4ca36281f5d4bdebe059b1187c2d36f699
Ariel Triana <ariel.triana88@gmail.com>
2018-02-18 22:50:48 -0500

    login (Form(template), remember())

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 declare function init_plugins();
 
@@ -10,13 +11,25 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
     init_plugins();
+
+    this.email = localStorage.getItem('email') || ''; // en caso de devolverme undefine retorno ''
   }
 
-  logIn() {
+  logIn(form: NgForm) {
+
+    if (form.value.remember === true) {
+      localStorage.setItem('email', form.value.email);
+    }
+
+    console.log(form.valid);
+    console.log(form.value);
+
     this.router.navigate(['/dashboard']);
   }
 
